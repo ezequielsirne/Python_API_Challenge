5f24dec31f61973793584ab04b7323d907e8a0d0
Ezequiel Sirne <ezequielsirne@gmail.com>
2025-05-26 18:40:50 -0300

    Reorder fields in CharacterEntity for improved readability

--- app/domain/entities/character_entity.py
+++ app/domain/entities/character_entity.py
@@ -3,11 +3,11 @@ from pydantic import BaseModel, Field
 
 class CharacterEntity(BaseModel):
     id: int = Field(..., strict=True)
+    name: str
     height: float = Field(..., strict=True)
     mass: float = Field(..., strict=True)
-    birth_year: int = Field(..., strict=True)
-    name: str
     hair_color: str
     skin_color: str
     eye_color: str
+    birth_year: int = Field(..., strict=True)
     

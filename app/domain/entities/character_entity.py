f0f094ce80125eb032c11c793ee3723d63960825
Ezequiel Sirne <ezequielsirne@gmail.com>
2025-05-26 18:39:37 -0300

    Refactor CharacterEntity fields to use Pydantic Field with strict validation

--- app/domain/entities/character_entity.py
+++ app/domain/entities/character_entity.py
@@ -2,12 +2,12 @@ from pydantic import BaseModel, Field
 
 
 class CharacterEntity(BaseModel):
-    id: int
+    id: int = Field(..., strict=True)
+    height: float = Field(..., strict=True)
+    mass: float = Field(..., strict=True)
+    birth_year: int = Field(..., strict=True)
     name: str
-    height: float
-    mass: float
     hair_color: str
     skin_color: str
     eye_color: str
-    birth_year: int
     
